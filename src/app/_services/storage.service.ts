fa8451db132b976c028ed08786c812cf34ac3b48
baiching <bachmarma@gmail.com>
2025-02-28 00:17:33 +0100

    it manages user's data in the browsers session

--- src/app/_services/storage.service.ts
+++ src/app/_services/storage.service.ts
@@ -31,7 +31,7 @@ export class StorageService {
   public isLoggedIn(): boolean {
     const user = window.sessionStorage.getItem(USER_KEY);
 
-    if (user){
+    if(user){
       return true;
     }
 
